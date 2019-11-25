eb6b7a9a077a81a6a9483c53b57b99b74955153f
Ruslan Lee <ruslan.lee@hotmail.com>
2019-11-25 14:27:24 +0700

    Commit build.gradle

--- src/test/java/id/co/ant/calculator/CalculatorTest.java
+++ src/test/java/id/co/ant/calculator/CalculatorTest.java
@@ -3,6 +3,8 @@ package id.co.ant.calculator;
 import org.junit.*;
 //import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
 
 	private Calculator calculator = new Calculator();
